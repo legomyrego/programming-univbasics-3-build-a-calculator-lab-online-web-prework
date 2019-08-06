59f26fa33213840d1a5142e1959b1357b647ecf2
Mark Rego <rego121@gmail.com>
2019-08-06 15:29:40 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,30 @@
 def addition(num1, num2)
-
+num1 + num2 = sum
+return sum
 end
 
 def subtraction(num1, num2)
-
+num1 - num2 = sum
+return sum
 end
 
 def division(num1, num2)
-
+num1/num2 = quotient
+return quotient
 end
 
 def multiplication(num1, num2)
-
+num1*num2 = product
+return product
 end
 
 def modulo(num1, num2)
+num1%num2 = modulo
+return modulo
 
 end
 
 def square_root(num)
-
+num.sqrt = square
+return square
 end
